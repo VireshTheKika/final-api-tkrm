4158d6406f2bc67b36f4cf41709ab92cbdee115f
VireshWork <patilviresh077@gmail.com>
2026-01-27 17:32:59 +0530

    error solved

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,13 +1,13 @@
+import express from "express";
 import {
   createClient,
   deleteClient,
   getClients,
 } from "../controllers/clientController.js";
-import express from "express";
 const clientRouter = express.Router();
 
-router.post("/add-client", createClient);
-router.get("/get-clients", getClients);
-router.delete("/delete-client/:id", deleteClient);
+clientRouter.post("/add-client", createClient);
+clientRouter.get("/get-clients", getClients);
+clientRouter.delete("/delete-client/:id", deleteClient);
 
 export default clientRouter;
